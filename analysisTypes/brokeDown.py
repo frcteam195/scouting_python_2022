cc0c95489bdcb7b51116b2076fbcf439a1eb16dd
aarbic <aaron.bicknell@snet.net>
2022-02-06 14:59:39 -0500

    Broke Down Script NEC

---
+++ analysisTypes/brokeDown.py
@@ -0,0 +1,55 @@
+import statistics
+import numpy as np
+
+
+# ******************** AnalysisTypeID = 9 = lostComm *******************
+
+def brokeDown(analysis, rsRobotMatches):
+    # Initialize the rsCEA record set and define variables specific to this function which lie outside the for loop
+    rsCEA = {}
+    rsCEA['AnalysisTypeID'] = 21
+    numberOfMatchesPlayed = 0
+
+    brokeDownList = []
+
+    # Loop through each match the robot played in.
+    for matchResults in rsRobotMatches:
+        rsCEA['Team'] = matchResults[analysis.columns.index('Team')]
+        rsCEA['EventID'] = matchResults[analysis.columns.index('EventID')]
+        autoDidNotShow = matchResults[analysis.columns.index('AutoDidNotShow')]
+        scoutingStatus = matchResults[analysis.columns.index('ScoutingStatus')]
+        # Skip if DNS or UR
+        if autoDidNotShow == 1:
+            rsCEA['Match' + str(matchResults[analysis.columns.index('TeamMatchNo')]) + 'Display'] = ''
+        elif scoutingStatus == 2:
+            rsCEA['Match' + str(matchResults[analysis.columns.index('TeamMatchNo')]) + 'Display'] = ''
+        else:
+            # Retrieve values from the matchResults and set to appropriate variables
+            brokeDown = matchResults[analysis.columns.index('SummBrokeDown')]
+            if brokeDown is None:
+                brokeDown = 999
+            if brokeDown == 0:
+                brokeDownString = 'N'
+            if brokeDown == 999:
+                brokeDownString = 'Error'
+            else:
+                brokeDownString = 'Y'
+
+            # Perform some calculations
+            numberOfMatchesPlayed += 1
+            brokeDownList.append(brokeDown)
+
+            # Create the rsCEA records for Dsiplay, Value, and Format
+            rsCEA['Match' + str(matchResults[analysis.columns.index('TeamMatchNo')]) + 'Display'] = brokeDownString
+            rsCEA['Match' + str(matchResults[analysis.columns.index('TeamMatchNo')]) + 'Value'] = brokeDown
+            if brokeDown == 0:
+                rsCEA['Match' + str(matchResults[analysis.columns.index('TeamMatchNo')]) + 'Format'] = 4
+            else:
+                rsCEA['Match' + str(matchResults[analysis.columns.index('TeamMatchNo')]) + 'Format'] = 2
+
+    # Create summary data
+    if numberOfMatchesPlayed > 0:
+        # Summary1 is the % of matches where they lost Comm
+        rsCEA['Summary1Display'] = round(np.sum(brokeDownList) / numberOfMatchesPlayed * 100, 1)
+
+    return rsCEA
