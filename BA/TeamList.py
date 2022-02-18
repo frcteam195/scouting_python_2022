39dada4574a66e86638881c27fa88325482f60df
Coolguy1782 <carmichall.braydon@gmail.com>
2022-02-17 20:22:05 -0500

    Made Multiple Changes to programs to allow programs to select databases

--- BA/TeamList.py
+++ BA/TeamList.py
@@ -3,6 +3,68 @@ import tbapy
 import xlsxwriter
 import sys
 import getopt
+import sys
+import argparse
+
+database = ''
+csvFilename = ''
+parser = argparse.ArgumentParser()
+parser.add_argument("-db", "--database", help = "Choices: aws-prod, aws-dev, pi-192, pi-10, localhost", required=True)
+args = parser.parse_args()
+input_database = args.database
+
+if input_database == "aws-prod":
+    database = "aws-prod"
+elif input_database == "aws-dev":
+    database = "aws-dev"
+elif input_database == "pi-192":
+    database = "pi-192"
+elif input_database == "pi-10":
+    database = "pi-10"
+elif input_database == "localhost":
+    database = "localhost"
+else:
+    print(input_database + " is not a invalid database choice. See --help for choices")
+    sys.exit()
+
+print ("Connecting to " + database)      
+
+def onlyascii(s):
+    return "".join(i for i in s if ord(i) < 128 and ord(i) != 39)
+
+if database == "aws-dev":
+        print("Input database " + input_database)
+        conn = mariaDB.connect(user='admin',
+                                    passwd='Einstein195',
+                                    host='frcteam195testinstance.cmdlvflptajw.us-east-1.rds.amazonaws.com',
+                                    database='team195_scouting')
+        cursor = conn.cursor()
+        
+elif database == "pi-10":
+        conn = mariaDB.connect(user='admin',
+                                passwd='team195',
+                                host='10.0.0.195',
+                                database='team195_scouting')
+        cursor = conn.cursor()
+
+elif database == "localhost":
+        conn = mariaDB.connect(user='admin',
+                                passwd='team195',
+                                host='localhost',
+                                database='team195_scouting')
+        cursor = conn.cursor()
+
+elif database == "aws-prod":
+        conn = mariaDB.connect(user='admin',
+                                passwd='Einstein195',
+                                host='frcteam195.cmdlvflptajw.us-east-1.rds.amazonaws.com',
+                                database='team195_scouting')
+        cursor = conn.cursor()
+
+else: 
+        print ("oops - Harish would not approve of that!")
+        sys.exit()
+
 tba = tbapy.TBA('Tfr7kbOvWrw0kpnVp5OjeY780ANkzVMyQBZ23xiITUkFo9hWqzOuZVlL3Uy6mLrz')
 x = 195
 team = tba.team(x)
@@ -11,18 +73,10 @@ def sortbyteam(d):
     return d.get('team_number', None)
 
 
-# Pi DB with remote access (e.g. from laptop)
-conn = mariaDB.connect(user='admin',
-                       passwd='team195',
-                       host='10.0.0.195',
-                       database='team195_scouting')
-cursor = conn.cursor()
-# Amazon devel DB
-# conn = mariaDB.connect(user='admin',
-#                        passwd='Einstein195',
-#                        host='frcteam195.cmdlvflptajw.us-east-1.rds.amazonaws.com',
-#                        database='team195_scouting')
-# cursor = conn.cursor()
+def wipeBAT():
+        cursor.execute("DELETE FROM BlueAllianceTeams;")
+        cursor.execute("ALTER TABLE BlueAllianceTeams AUTO_INCREMENT = 1;")
+        conn.commit()
 
 teamList = []
 cursor.execute("SELECT Events.BAEventID FROM Events WHERE Events.CurrentEvent = 1;")
@@ -36,8 +90,7 @@ if len(sys.argv) != 2:
 else:
     args = getopt.getopt(sys.argv,"")[1][1]
     if args == 'db':
-        cursor.execute("DELETE FROM BlueAllianceTeams")
-        conn.commit()
+        wipeBAT()
 
         eventTeams = tba.event_teams(event)
         for team in sorted(eventTeams, key=sortbyteam):
@@ -52,6 +105,7 @@ else:
             query = "INSERT INTO BlueAllianceTeams (Team, TeamName, TeamLocation) VALUES " + "('" + str(team.team_number) + \
                      "','" + tempNick + "','" + str(cityState) + "');"
             # print(query)
+
             cursor.execute(query)
             conn.commit()
 
