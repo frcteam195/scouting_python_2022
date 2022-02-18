93aec6830d9965211f5e398e2fb04388b95f240d
VeryCoolandneat <joefrechette040@gmail.com>
2022-02-17 19:15:49 -0800

    Fixed summGroundPickup

--- analysisTypes/summGroundPickup.py
+++ analysisTypes/summGroundPickup.py
@@ -1,53 +1,60 @@
-import numpy as np
-
+import statistics
 
 def summGroundPickup(analysis, rsRobotMatches):
+   # start = time.time()
+    # print("teleop time:")
     # Initialize the rsCEA record set and define variables specific to this function which lie outside the for loop
     rsCEA = {}
     rsCEA['AnalysisTypeID'] = 40
     numberOfMatchesPlayed = 0
+    summGroundPickupList  = []
 
-    groundPickupList  = []
 
     # Loop through each match the robot played in.
     for matchResults in rsRobotMatches:
+        # This is sort of dumb as rsCEA Team and EventID will be overwritten for each match, but easier
+        #   to overwite it up to 12 times than to fix at this time.
         rsCEA['Team'] = matchResults[analysis.columns.index('Team')]
         rsCEA['EventID'] = matchResults[analysis.columns.index('EventID')]
-        autoDidNotShow = matchResults[analysis.columns.index('AutoDidNotShow')]
         scoutingStatus = matchResults[analysis.columns.index('ScoutingStatus')]
-        # Skip if DNS or UR
-        if autoDidNotShow == 1:
-            rsCEA['Match' + str(matchResults[analysis.columns.index('TeamMatchNo')]) + 'Display'] = ''
-        elif scoutingStatus == 2:
-            rsCEA['Match' + str(matchResults[analysis.columns.index('TeamMatchNo')]) + 'Display'] = ''
+        if scoutingStatus == 2:
+            rsCEA['Match' + str(matchResults[analysis.columns.index('TeamMatchNo')]) + 'Display'] = 'UR'
         else:
             # Retrieve values from the matchResults and set to appropriate variables
-            groundPickup = matchResults[analysis.columns.index('SummGroundPickup')]
-            if groundPickup is None:
-                groundPickup = 999
-            if groundPickup == 0:
-                groundPickupString = 'No'
-            if groundPickup == 999:
-                groundPickupString = 'Error'                
-            else:
-                groundPickupString = 'Yes'
+            summGroundPickup = matchResults[analysis.columns.index('SummGroundPickup')]
+            if summGroundPickup is None:
+                summGroundPickup = 999
+            if summGroundPickup == 0:
+                summGroundPickupDisplay = 'N'
+                summGroundPickupFormat = 2
+                summGroundPickupValue = 0
+                summGroundPickupList.append(summGroundPickupValue)
+            if summGroundPickup == 1:
+                summGroundPickupDisplay = 'Y'
+                summGroundPickupFormat = 4
+                summGroundPickupValue = 1   
+                summGroundPickupList.append(summGroundPickupValue)
 
             # Perform some calculations
-            numberOfMatchesPlayed += 1
-            groundPickupList.append(groundPickup)
+            numberOfMatchesPlayed += 1     
 
-            # Create the rsCEA records for Dsiplay, Value, and Format
-            rsCEA['Match' + str(matchResults[analysis.columns.index('TeamMatchNo')]) + 'Display'] = groundPickupString
-            rsCEA['Match' + str(matchResults[analysis.columns.index('TeamMatchNo')]) + 'Value'] = groundPickup
-            if groundPickup == 0:
-                rsCEA['Match' + str(matchResults[analysis.columns.index('TeamMatchNo')]) + 'Format'] = 2
-            else:
-                rsCEA['Match' + str(matchResults[analysis.columns.index('TeamMatchNo')]) + 'Format'] = 4
-
+            # Create the rsCEA records for Display, Value, and Format
+            rsCEA['Match' + str(matchResults[analysis.columns.index('TeamMatchNo')]) + 'Display'] = \
+                    str(summGroundPickupDisplay)
+            rsCEA['Match' + str(matchResults[analysis.columns.index('TeamMatchNo')]) + 'Value'] = summGroundPickupValue
+            rsCEA['Match' + str(matchResults[analysis.columns.index('TeamMatchNo')]) + 'Format'] = summGroundPickupFormat
 
     # Create summary data
     if numberOfMatchesPlayed > 0:
-        # Summary1 is the % of matches where they performed a Ground Pickup
-        rsCEA['Summary1Display'] = (round(np.sum(groundPickupList) / numberOfMatchesPlayed * 100))
+        rsCEA['Summary1Display'] = round(statistics.mean(summGroundPickupList), 2)
+        rsCEA['Summary1Value'] = round(statistics.mean(summGroundPickupList), 2)
+        # Some test code for calculating min, max, quantiles
+        #print(min(totalBallsList))
+        #print(max(totalBallsList))
+        #testList = [22, 33, 44, 23, 43, 56, 43, 56, 76, 99, 23, 1, 109, 34, 76, 89, 99, 23, 55]
+        #print(np.quantile(testList, 0.25))
+
+    # end = time.time()
+    # print(end - start)
 
     return rsCEA
