588c4f5aad901c6a901e624766cc97b9b8c69a71
Mark Maciejewski <mark.maciejewski@gmail.com>
2022-03-08 17:32:25 -0500

    renamed loop script

--- run-analysis-loop.sh
+++ run-analysis-loop.sh
@@ -5,8 +5,7 @@ i=1
 while [ $i -le  10 ]
 do
     echo "Running $i of 10"
-    python3 analysisIR.py -db aws-dev
-    python3 graphIR.py -db aws-dev
+    ./run-analysis.sh
     sleep 60
     i=$(( $i + 1 ))
 done
