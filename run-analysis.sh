740a4f3dd43f5439b78fc9982c91ce9b476b5be8
Mark Maciejewski <mark.maciejewski@gmail.com>
2022-03-12 10:34:04 -0500

    asdf

--- run-analysis.sh
+++ run-analysis.sh
@@ -1,7 +1,10 @@
 #! /usr/bin/bash
 
 echo 'Running analysisIR'
-/usr/bin/python3 /home/pi/scouting_python_2022/analysisIR.py -db aws-dev
+/usr/bin/python3 /home/pi/scouting_python_2022/analysisIR.py -db localhost
 
 echo 'Running graphIR'
-/usr/bin/python3 /home/pi/scouting_python_2022/graphIR.py -db aws-dev
+/usr/bin/python3 /home/pi/scouting_python_2022/graphIR.py -db localhost
+
+echo 'Running copyTable'
+/usr/bin/python3 /home/pi/scouting_python_2022/copyTable.py -dbs localhost -dbd aws-dev -table MotorTypes
