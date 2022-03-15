952b531b56159b52b88f0a58aa2f795c082ddbb0
Mark Maciejewski <mark.maciejewski@gmail.com>
2022-03-15 19:14:31 -0400

    sdf

--- run-analysis.sh
+++ run-analysis.sh
@@ -2,12 +2,6 @@
 
 echo '**********************************************************'
 
-echo 'Running analysisIR'
-/usr/bin/python3 /home/pi/scouting_python_2022/analysisIR.py -db aws-dev
-
-echo 'Running graphIR'
-/usr/bin/python3 /home/pi/scouting_python_2022/graphIR.py -db aws-dev
-
 echo 'Running BA OPRs'
 /usr/bin/python3 /home/pi/scouting_python_2022/BA/Oprs.py -db aws-dev
 
@@ -15,3 +9,11 @@ echo ''
 
 echo 'Running BA Ranks'
 /usr/bin/python3 /home/pi/scouting_python_2022/BA/Ranks.py -db aws-dev
+
+
+echo 'Running analysisIR'
+/usr/bin/python3 /home/pi/scouting_python_2022/analysisIR.py -db aws-dev
+
+echo 'Running graphIR'
+/usr/bin/python3 /home/pi/scouting_python_2022/graphIR.py -db aws-dev
+
