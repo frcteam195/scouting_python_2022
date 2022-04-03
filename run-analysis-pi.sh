72b89eaec47ba25c407c24800c4a36ad378af4f2
Chris Bontempi <chrisbontempi@compuserve.com>
2022-04-03 18:01:59 -0400

    asdf

--- run-analysis-pi.sh
+++ run-analysis-pi.sh
@@ -29,7 +29,7 @@ echo 'Running graphIR'
 /usr/bin/python3 /home/pi/scouting_python_2022/graphIR.py -db localhost
 
 echo 'Running mysqldump'
-/usr/bin/mysqldump --defaults-extra-file=/home/pi/my-pi.cnf team195_scouting > /home/pi/DB-backups/dbdump.sql
+/usr/bin/mysqldump -u admin -pteam195 team195_scouting > /home/pi/DB-backups/dbdump.sql
 /bin/sleep 1
 #/bin/tar -czf dbdump.sql.tgz
 #/bin/sleep 1
@@ -37,5 +37,5 @@ echo ''
 
 echo 'Running mysql to copy database to AWS-dev'
 db=frcteam195testinstance.cmdlvflptajw.us-east-1.rds.amazonaws.com
-/usr/bin/mysql --defaults-extra-file=/home/pi/my-aws.cnf -h $db team195_scouting < /home/pi/DB-backups/dbdump.sql
+/usr/bin/mysql -h $db -u admin -pEinstein195 team195_scouting < /home/pi/DB-backups/dbdump.sql
 echo ''
