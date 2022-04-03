81938e278cdcb873f164fda2d06ffdd41de3838a
Chris Bontempi <chrisbontempi@compuserve.com>
2022-04-03 18:23:08 -0400

    sdf

--- run-analysis-pi.sh
+++ run-analysis-pi.sh
@@ -4,23 +4,18 @@ echo '**********************************************************'
 
 echo 'Running BA OPRs'
 /usr/bin/python3 /home/pi/scouting_python_2022/BA/Oprs.py -db localhost
-echo ''
 
 echo 'Running BA Ranks'
 /usr/bin/python3 /home/pi/scouting_python_2022/BA/Ranks.py -db localhost
-echo ''
 
 echo 'Running BA MatchData'
 /usr/bin/python3 /home/pi/scouting_python_2022/BA/MatchData.py -db localhost
-echo ''
 
 echo 'Running copyBAMatchData'
 /usr/bin/python3 /home/pi/scouting_python_2022/copyBAMatchData.py -db localhost
-echo ''
 
 echo 'Running SheetsLvl2Scouting'
 /usr/bin/python3 /home/pi/scouting_python_2022/SheetsLvl2Scouting.py -db localhost
-echo ''
 
 echo 'Running analysisIR'
 /usr/bin/python3 /home/pi/scouting_python_2022/analysisIR.py -db localhost
@@ -28,14 +23,22 @@ echo 'Running analysisIR'
 echo 'Running graphIR'
 /usr/bin/python3 /home/pi/scouting_python_2022/graphIR.py -db localhost
 
-echo 'Running mysqldump'
+echo 'Running mysqldump to dump local DB to dbdump.sql'
+start_time=$(date +%s)
 /usr/bin/mysqldump -u admin -pteam195 team195_scouting > /home/pi/DB-backups/dbdump.sql
 /bin/sleep 1
+end_time=$(date +%s)
+elapsed=$(( end_time - start_time ))
+echo "Time: $elapsed seconds"
 #/bin/tar -czf dbdump.sql.tgz
 #/bin/sleep 1
 echo ''
 
 echo 'Running mysql to copy database to AWS-dev'
+start_time=$(date +%s)
 db=frcteam195testinstance.cmdlvflptajw.us-east-1.rds.amazonaws.com
 /usr/bin/mysql -h $db -u admin -pEinstein195 team195_scouting < /home/pi/DB-backups/dbdump.sql
+end_time=$(date +%s)
+elapsed=$(( end_time - start_time ))
+echo "Time: $elapsed seconds"
 echo ''
